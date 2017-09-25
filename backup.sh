1c3a8c3675b59a003888088f4463dce18664ba04
Bebabo10 <Nylegarcia01@gmail.com>
2017-09-25 14:17:31 -0500

    Update backup.sh
    updated for borg

--- backup.sh
+++ backup.sh
@@ -11,40 +11,49 @@ backup(){
 
 if [$debugging == 0]; then
 	echo "_+=------------------------------------=+_"
-	echo "Starting Backup: `date`"
+	echo "Starting Backup: " $t0
 	echo "Sending tmux commands to $SERVER server!"
 	sudo -u $USER tmux send-keys -t $SERVER "say Starting Server Backup" ENTER
 	echo "Sending save-all command to tmux session"
 	sudo -u $USER tmux send-keys -t $SERVER "save-all" ENTER
-	echo "Turning world save off for rsync backup"
+	echo "Turning world save off for backup"
 	sudo -u $USER tmux send-keys -t $SERVER "save-off" ENTER
-	echo "Running rsync as user[ $USER ] on server [ $SERVER ] ..."
-	sudo -u $USER rsync -azP -H --delete --numeric-ids /opt/$SERVER /opt/backups/$SERVER
+	echo "Running backup as user[ $USER ] on server [ $SERVER ] ..."
+	#Making incremental backup using attic
+	sudo -u $USER borg create -v --stats /opt/backups/$SERVER/$SERVER::$t0 /opt/$SERVER 
 	echo "Turning world save back on "
 	sudo -u $USER tmux send-keys -t $SERVER "save-on" ENTER
 	sudo -u $USER tmux send-keys -t $SERVER "say Server Backup Complete" ENTER
+	echo "Running prune job to keep space available"
+	# Keep all backups in the last 10 days, 4 additional end of week archives,
+	# and an end of month archive for every month:
+	sudo -u $USER borg prune   -d 1 -w 1 -m 1 -y 1 --keep-within=1d /opt/backups/$SERVER
 	echo "Backup Complete: `date`"
-	echo "_+=------------------------------------=+_"	
+	echo "_+=------------------------------------=+_"
 fi
 
 if [$debugging == 1]; then
-	echo "_+=------------------------------------=+_" >> backuplog.txt
-	echo "Starting Backup: `date`" >> backuplog.txt
+	echo "_+=------------------------------------=+_" >> backuplog.txt	
+	echo "Starting Backup: " $t0 >> backuplog.txt	
 	echo "Sending tmux commands to $SERVER server!"
 	sudo -u $USER tmux send-keys -t $SERVER "say Starting Server Backup" ENTER
 	echo "Sending save-all command to tmux session"
 	sudo -u $USER tmux send-keys -t $SERVER "save-all" ENTER
-	echo "Turning world save off for rsync backup"
+	echo "Turning world save off for backup"
 	sudo -u $USER tmux send-keys -t $SERVER "save-off" ENTER
-	echo "Running rsync as user[ $USER ] on server [ $SERVER ] ..."
-	sudo -u $USER rsync -azP -H --delete --numeric-ids /opt/$SERVER /opt/backups/$SERVER
+	echo "Running backup as user[ $USER ] on server [ $SERVER ] ..."
+	#Making incremental backup using attic
+	sudo -u $USER borg create -v --stats /opt/backups/$SERVER/$SERVER::$t0 /opt/$SERVER 
 	echo "Turning world save back on "
 	sudo -u $USER tmux send-keys -t $SERVER "save-on" ENTER
 	sudo -u $USER tmux send-keys -t $SERVER "say Server Backup Complete" ENTER
-	echo "Backup Complete: `date`" >> backuplog.txt
-	echo "_+=------------------------------------=+_" >> backuplog.txt
-		
-fi
+	echo "Running prune job to keep space available"
+	# Keep all backups in the last 10 days, 4 additional end of week archives,
+	# and an end of month archive for every month:
+	sudo -u $USER borg prune   -d 1 -w 1 -m 1 -y 1 --keep-within=1d /opt/backups/$SERVER
+	echo "Backup Complete: `date`" >> backuplog.txt	
+	echo "_+=------------------------------------=+_" >> backuplog.txt	
+	fi
 }
 
 while getopts s:u:f: option
