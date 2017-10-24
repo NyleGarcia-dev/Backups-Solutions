1f2cb7a23bb3e9a1e1969ce97cc459f9e2135a46
Bebabo10 <Nylegarcia01@gmail.com>
2017-10-24 09:42:40 -0500

    fixed

--- backup.sh
+++ backup.sh
@@ -1,83 +1,128 @@
 #!/bin/bash
-Backupdir=""
-Source=""
+SERVER="0"
+USER="minecraft"
+fname=./backup.conf
 t0=`date +%FT%H%M%S`;
-bktime=1m
-
+DEBUGGING=0
+RETAIN_NUM_LINES=3100
+LOGFILE=backuplog.txt
+#rm -f backup.txt 
 
 
 dircheck(){
 
-	if [ -d "$Backupdir" ]; 
-		then 
-		 
-		 
-		else
-			 mkdir -p $Backupdir
-			 borgint
+	if [ -d "/opt/backups/$SERVER" ]; 
+		then
+
+		else 
+			sudo -u $USER mkdir -p /opt/backups/$SERVER
+			borgint
 	fi
 
 
 	
 }
 borgint(){
-	 borg init --encryption=none $Backupdir
+	sudo -u $USER borg init --encryption=none /opt/backups/$SERVER/$SERVER
 	t0="Firstrun"
 }
 startbk(){
   	echo "_+=------------------------------------=+_" 	
 	echo "Starting Backup: " $t0	
-	
+	echo "Sending tmux commands to $SERVER server!"
+	sudo -u $USER tmux send-keys -t $SERVER "say Starting Server Backup" ENTER
 }
 endbk(){
+	sudo -u $USER tmux send-keys -t $SERVER "say Server Backup Complete" ENTER
 	echo "Backup Complete: `date`"	
 	echo "_+=------------------------------------=+_"
 
 }
-
-runbackup(){
-	echo "Backing up source[ $Source ] to [ $Backupdir ] ..."
-	#Making incremental backup using borg
-	 borg create -v --stats $Backupdir::$t0 $Source 
+saveall(){
+	echo "Sending save-all command to tmux session"
+	sudo -u $USER tmux send-keys -t $SERVER "save-all" ENTER
 
 }
+saveoff(){
+	echo "Turning world save off for backup"
+	sudo -u $USER tmux send-keys -t $SERVER "save-off" ENTER
 
+}
+runbackup(){
+	echo "Running backup as user[ $USER ] on server [ $SERVER ] ..."
+	#Making incremental backup using attic
+	sudo -u $USER borg create -v --stats /opt/backups/$SERVER/$SERVER::$t0 /opt/$SERVER 
+
+}
+saveon(){
+	echo "Turning world save back on "
+	sudo -u $USER tmux send-keys -t $SERVER "save-on" ENTER
+}
 prune(){
 	echo "Running prune job to keep space available"
 	# Keep all backups in the last 10 days, 4 additional end of week archives,
 	# and an end of month archive for every month:
-	 borg prune -v --list --keep-within=1d   --keep-daily=7 --keep-weekly=4  --keep-monthly=1 $Backupdir
+	sudo -u $USER borg prune -v --list --keep-within=1d   --keep-daily=7 --keep-weekly=4  --keep-monthly=1 /opt/backups/$SERVER/$SERVER
 }
 backup(){
+
 	dircheck
 	startbk 
+	saveall 
+	saveoff
 	runbackup 
+	saveon
 	prune
 	endbk 
 }
 
 
-while getopts s:d: option
+while getopts s:u:f: option
 do
  case "${option}"
   in
-  s) Source=${OPTARG:-"0"};;
-  d) Backupdir=${OPTARG:-""};;
- esac 
-
- 
+  s) SERVER=${OPTARG:-"0"};;
+  u) USER=${OPTARG:-"minecraft"};;
+  f) fname=${OPTARG:-"${DEFAULTBACKUPFILE}"};;
+ esac
 done
 
 
-while :
-do
 
 
-backup
+if [[ $SERVER = "0" ]]; 
+then
+echo "  $SERVER - - - $USER  ---- $fname "
+	while read args; do
+		echo "${args}"
+		IFS=' ' read -a myarray <<< "${args}"
+		index=0
+		
+		for i in ${myarray[@]};
+		do 
+			if [[ $i = "-u" ]]; 
+			then
+				USER=${myarray[ index + 1 ]}
+		
+			fi
+			
+			if [[ $i = "-s" ]]; 
+			then
+				SERVER=${myarray[ index + 1 ]}
+			fi
+
+			index=$((index+1))
+		done
+		
+		echo "Backing up -u $USER -s $SERVER "
+		backup
+		
+	done <${fname}
+
+else
+	backup
+	
+fi
 echo "  "
 echo "  "
 echo "  "
-sleep $bktime
-done
-
-
