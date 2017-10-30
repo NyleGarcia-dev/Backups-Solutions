f4bd76e5d23baabb4e0c2a5201eb5c845317dfcd
NGarcia01 <Nylegarcia01@gmail.com>
2017-10-30 12:59:28 -0500

    Create runbackup.sh

---
+++ runbackup.sh
@@ -0,0 +1,5 @@
+
+sh /c/Scripts/backup.sh -s /c/Users/ -d /z/users
+sh /c/Scripts/backup.sh -s /c/Program\ Files/ -d /z/Program\ Files/
+sh /c/Scripts/backup.sh -s /c/Program\ Files\ \(x86\)/ -d /z/Program\ Files\ \(x86\)/
+sh /c/Scripts/backup.sh -s /c/windows/ -d /z/windows
